82fecc4e5ccbfa51c2431d6c4971bdd8083fedfb
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-02-14 16:47:53 -0800

    te - began working on constraint solver to generate values from specified conditions (to be used in future consistency checker)

--- Grammar Fuzzer/test.py
+++ Grammar Fuzzer/test.py
@@ -1,27 +1,30 @@
 from fuzzingbook.GrammarFuzzer import GrammarFuzzer
 from typing import Dict, Union, Any, Tuple, List
 import string
+import random
+import sys
+
+# Specify database configuration
+
+db = 'Users_DB'
+fields = ['name', 'age', 'email_address', 'phone_number']
+# TODO: Allow passing of params to specify additional constraints (e.g., numerical ranges, age should be 1 to 100)
+types = ['<String>', '<Integer>', '<Email>', '<Phone>']
+comparators = ['<StringComparator>', '<Comparator>',
+               '<StringComparator>', '<StringComparator>']
+
+# Specify SQL Grammar for grammar-based fuzzer to generate SQL queries
 
 Option = Dict[str, Any]
 Expansion = Union[str, Tuple[str, Option]]
 Grammar = Dict[str, List[Expansion]]
 
-dbs = {
-    'Users_DB': {
-        'fields': ['name', 'age', 'email_address', 'phone_number'],
-        # TODO: Allow passing of params to specify additional constraints (e.g., numerical ranges, age should be 1 to 100)
-        'types': ['<String>', '<Integer>', '<Email>', '<Phone>'],
-    }
-}
-
-DB = 'Users_DB'
-
 SQL_GRAMMAR: Grammar = {
     '<Query>':
-        ['SELECT <SelList> FROM <FromList> WHERE <Condition>',
-         'SELECT <SelList> FROM <FromList>',
-         'SELECT * FROM <FromList>',
-         'INSERT INTO <FromList> VALUES ("' + '", "'.join(dbs[DB]["types"]) + '")'],
+        ['SELECT <SelList> FROM <FromList> WHERE <Condition>'],
+    #  'SELECT <SelList> FROM <FromList>',
+    #  'SELECT * FROM <FromList>',
+        #  'INSERT INTO <FromList> VALUES ("' + '", "'.join(types) + '")'],
 
     '<SelList>':
         ['<Attribute>', '<SelList>, <Attribute>'],
@@ -34,16 +37,22 @@ SQL_GRAMMAR: Grammar = {
             '<Condition> OR <Comparison>'],
 
     '<Comparison>':
-        [f'{f} <Comparator> "{t}"' for f, t in zip(
-            dbs[DB]['fields'], dbs[DB]['types'])],
+        [f'{f} {c} "{t}"' for f, c, t in zip(fields, comparators, types)],
 
     '<Comparator>':
-        ['<', '<=', '=', '>=', '>'],
+        ['<', '<=', '=', '<LAngle><RAngle>' '>=', '>'],
 
-    '<Relation>': [DB],
+    '<StringComparator>':
+        ['=', '<LAngle><RAngle>'],
+
+    '<LAngle>': ['<'],
+
+    '<RAngle>': ['>'],
+
+    '<Relation>': [db],
 
     '<Attribute>':
-        dbs[DB]['fields'],
+        fields,
 
     # Types:
     '<String>': ['<Char>', '<String><Char>'],
@@ -61,5 +70,62 @@ SQL_GRAMMAR: Grammar = {
 fuzzer = GrammarFuzzer(grammar=SQL_GRAMMAR,
                        start_symbol='<Query>', max_nonterminals=5)
 
-for i in range(15):
-    print(f'#{i}: \t{fuzzer.fuzz()}')
+
+def extract_type(sql):
+    return sql[:sql.find(' ')]
+
+
+def extract_constraints(sql):
+    # Get full condition (everything after 'WHERE')
+    condition_start = sql.find('WHERE')
+    cond = sql[condition_start+6:] if condition_start != -1 else ''
+
+    # Separate constraint possibilities
+    constraints = cond.split(' OR ')
+    if constraints == ['']:
+        constraints = [cond]
+
+    return constraints
+
+
+def generate_value_from_constraints(constraints):
+    constraint_table = dict([(field, {'max': None, 'min': None, 'eq': None, 'neq': []}) if comp == '<Comparator>'
+                             else (field, {'eq': None, 'neq': []})
+                             for field, comp in zip(fields, comparators)])
+
+    constraint = random.choice(constraints)
+
+    # for conditions in constraint.split('AND'):
+    #     for c in conditions:
+    #         field, comp, val = c
+    #         val = val[1:-1] # Remove extraneous quotes
+
+    #         if comp == '<Comparison>':  # Integer
+    #             if comp == '<':
+    #                 constraint_table[field]['max'] = min(constraint_table[field]['max'], val - 1) if constraint_table[field]['max'] else val - 1
+    #             if comp == '<=':
+    #                 constraint_table[field]['max'] = min(constraint_table[field]['max'], val) if constraint_table[field]['max'] else val
+    #             if comp == '=':
+    #                 if constraint_table[field]['eq'] and constraint_table[field]['eq'] != val:
+    #                     print('INVALID CONSTRAINTS: specified multiple non-equivalent equals')
+    #                     return
+    #                 else:
+
+    #             if comp == '<>':
+    #             if comp == '>=':
+    #                 constraint_table[field]['min'] = max(constraint_table[field]['min'], val) if constraint_table[field]['min'] else val
+    #             if comp == '>':
+    #                 constraint_table[field]['min'] = min(constraint_table[field]['min'], val + 1) if constraint_table[field]['min'] else val + 1
+    #             constraint_table[]
+
+    #         elif comp == '<StringComparison>':  # String
+
+    print(constraint_table)
+
+    return
+
+
+for i in range(5):
+    sql = fuzzer.fuzz()
+    print(f'#{i}: \t{sql}')
+    c = generate_value_from_constraints(extract_constraints(sql))
