16f0ac7e913cf8e131725aafbc4093e1a3bf9c24
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-02-14 20:15:15 -0800

    te - finished preliminary function to generate constraints from a set of conditions specified in a SQL query

--- Grammar Fuzzer/test.py
+++ Grammar Fuzzer/test.py
@@ -75,57 +75,86 @@ def extract_type(sql):
     return sql[:sql.find(' ')]
 
 
-def extract_constraints(sql):
+def extract_conditions(sql):
     # Get full condition (everything after 'WHERE')
     condition_start = sql.find('WHERE')
     cond = sql[condition_start+6:] if condition_start != -1 else ''
 
     # Separate constraint possibilities
-    constraints = cond.split(' OR ')
-    if constraints == ['']:
-        constraints = [cond]
+    conditions = cond.split(' OR ')
+    if conditions == ['']:
+        conditions = [cond]
 
-    return constraints
+    return conditions
 
 
-def generate_value_from_constraints(constraints):
+def generate_constraints_from_conditions(conditions):
     constraint_table = dict([(field, {'max': None, 'min': None, 'eq': None, 'neq': []}) if comp == '<Comparator>'
                              else (field, {'eq': None, 'neq': []})
                              for field, comp in zip(fields, comparators)])
 
-    constraint = random.choice(constraints)
+    condition = random.choice(conditions)
 
-    # for conditions in constraint.split('AND'):
-    #     for c in conditions:
-    #         field, comp, val = c
-    #         val = val[1:-1] # Remove extraneous quotes
+    for terms in condition.split('AND'):
+        # print('Terms:', terms)
+        # print('Terms (split):', terms.strip().split(' '))
+        field, comp, val = terms.strip().split(' ')
+        val = val[1:-1]  # Remove extraneous quotes
 
-    #         if comp == '<Comparison>':  # Integer
-    #             if comp == '<':
-    #                 constraint_table[field]['max'] = min(constraint_table[field]['max'], val - 1) if constraint_table[field]['max'] else val - 1
-    #             if comp == '<=':
-    #                 constraint_table[field]['max'] = min(constraint_table[field]['max'], val) if constraint_table[field]['max'] else val
-    #             if comp == '=':
-    #                 if constraint_table[field]['eq'] and constraint_table[field]['eq'] != val:
-    #                     print('INVALID CONSTRAINTS: specified multiple non-equivalent equals')
-    #                     return
-    #                 else:
+        if comparators[fields.index(field)] == '<Comparator>':  # Integer
+            val = int(val)
+            if comp == '<':
+                # TODO: Add validity checking (e.g., warn/terminate if less than and greater than values have no overlapping range)
+                constraint_table[field]['max'] = min(
+                    constraint_table[field]['max'], val - 1) if constraint_table[field]['max'] else val - 1
+            elif comp == '<=':
+                constraint_table[field]['max'] = min(
+                    constraint_table[field]['max'], val) if constraint_table[field]['max'] else val
+            elif comp == '=':
+                if constraint_table[field]['eq'] and constraint_table[field]['eq'] != val:
+                    print(
+                        'INVALID CONSTRAINTS: specified multiple non-equivalent equals')
+                    return
+                else:
+                    constraint_table[field]['eq'] = val
+            elif comp == '<>':
+                if val not in constraint_table[field]['neq']:
+                    constraint_table[field]['neq'].append(val)
+            elif comp == '>=':
+                constraint_table[field]['min'] = max(
+                    constraint_table[field]['min'], val) if constraint_table[field]['min'] else val
+            elif comp == '>':
+                constraint_table[field]['min'] = min(
+                    constraint_table[field]['min'], val + 1) if constraint_table[field]['min'] else val + 1
 
-    #             if comp == '<>':
-    #             if comp == '>=':
-    #                 constraint_table[field]['min'] = max(constraint_table[field]['min'], val) if constraint_table[field]['min'] else val
-    #             if comp == '>':
-    #                 constraint_table[field]['min'] = min(constraint_table[field]['min'], val + 1) if constraint_table[field]['min'] else val + 1
-    #             constraint_table[]
+        elif comparators[fields.index(field)] == '<StringComparator>':  # String
+            if comp == '=':
+                if constraint_table[field]['eq'] and constraint_table[field]['eq'] != val:
+                    print(
+                        'INVALID CONSTRAINTS: specified multiple non-equivalent equals')
+                    return
+                else:
+                    constraint_table[field]['eq'] = val
+            elif comp == '<>':
+                if val not in constraint_table[field]['neq']:
+                    constraint_table[field]['neq'].append(val)
 
-    #         elif comp == '<StringComparison>':  # String
+    # print(constraint_table)
+    return constraint_table
 
-    print(constraint_table)
 
+def generate_values_from_constraints(constraints):
+    # TODO
     return
 
 
-for i in range(5):
+def generate_values(sql):
+    conds = extract_conditions(sql)
+    constraints = generate_constraints_from_conditions(conds)
+    return generate_values_from_constraints(constraints)
+
+
+for i in range(3):
     sql = fuzzer.fuzz()
     print(f'#{i}: \t{sql}')
-    c = generate_value_from_constraints(extract_constraints(sql))
+    c = generate_constraints_from_conditions(extract_conditions(sql))
